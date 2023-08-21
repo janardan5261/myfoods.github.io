fac6fc8f938123763e897c781ad7151706ac9fcb
janardan shirsath <janardhanshirsath020@gmail.com>
2023-08-21 12:01:44 +0530

    It`s my commit

---
+++ contact.php
@@ -0,0 +1,119 @@
+<?php
+include_once("include/heder.php");
+?>
+
+
+<section class="breadcrumb-area" style="background-image:url(images/background/2.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="breadcrumbs text-center">
+                    <h1>Contact us</h1>
+                    <h4>Welcome to certified online organic products suppliersr</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="breadcrumb-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-5 col-sm-5">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                        <li>Contact us</li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-7 col-sm-7">
+                    <p>We provide <span>100% organic</span> products</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="single-contact_us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="left_contact">
+                    <h5>Get In Touch</h5>
+                    <ul class="list catagories">
+                        <li><a href="#"><i class="fa fa-home color1"></i>No 271, Red Cross Building, Modern Street,
+                                Newyork City, USA.</a></li>
+                        <li><a href="#"><i
+                                    class="fa fa-envelope color1"></i><span>Organicstore@gmail.com</span><br>customersupport@organic.com</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-phone color1"></i>+91 (321) 758 142 5698
+                                <br>1800-8692-258-1547</a></li>
+                    </ul>
+
+                    <div class="border-area">
+                        <h6>Woriking Hours</h6>
+                        <div class="list Business">
+                            <p>Monday - Friday: 09.00am to 07.00pm <br>Saturday: 10.00am to 05.00pm <br>Sunday:
+                                <span>Closed</span></p>
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <div class="contact_in-box">
+                    <div class="theme-title ">
+                        <h2>send us massege</h2>
+                    </div>
+                    <form action="https://st.ourhtmldemo.com/template/organic_store/inc/sendmail.php">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="text" placeholder="Your Name*">
+                            </div>
+                            <!-- /.col-md-6 -->
+                            <div class="col-md-6">
+                                <input type="text" placeholder="Your Email*">
+                            </div>
+                            <!-- /.col-md-6 -->
+                            <div class="col-md-6">
+                                <input type="text" placeholder="Phone">
+                            </div>
+                            <!-- /.col-md-6 -->
+                            <div class="col-md-6">
+                                <input type="text" placeholder="Subject">
+                            </div>
+                            <!-- /.col-md-6 -->
+
+                            <div class="col-md-12">
+                                <textarea placeholder="Comments"></textarea>
+                            </div>
+                            <!-- /.col-md-12 -->
+                            <div class="col-md-12">
+                                <button type="submit" class="color1_bg">Post Comment</button>
+                            </div>
+                            <!-- /.col-md-12 -->
+                        </div>
+                        <!-- /.row -->
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Google map************************ -->
+<section id="google-map-area">
+    <div class="google-map-home" id="google-map" data-map-lat="40.717873" data-map-lng="-73.563033"
+        data-icon-path="images/logo/map.png" data-map-title="Awesome Place" data-map-zoom="11"></div>
+</section>
+
+
+
+
+<?php
+include_once("include/footer.php");
+?>
---
+++ include/heder.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from st.ourhtmldemo.com/template/organic_store/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 Aug 2023 13:26:40 GMT -->
+
+<head>
+    <meta charset="UTF-8">
+    <!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- For Resposive Device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Organic Store </title>
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="images/fav-icon/apple-icon-57x57.png">
+
+
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+
+    <!-- Fixing Internet Explorer ______________________________________-->
+
+    <!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="vendor/html5shiv.js"></script>
+		<![endif]-->
+</head>
+
+
+
+<body>
+    <div class="main_page">
+
+
+        <!-- Header *******************************  -->
+        <header>
+            <div class="top_header">
+                <div class="container">
+                    <div class="pull-left header_left">
+                        <ul>
+                            <li><a href="#">Order On Phone: <span>+321 453 68 739</span></a></li>
+                            <li><i class="fa fa-envelope-o s_color" aria-hidden="true"></i><a
+                                    href="#">organicstore@gmail.com</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="pull-right header_right">
+                        <div class="state" id="value1">
+                            <ul>
+                                <li><i class="fa fa-user s_color" aria-hidden="true"></i><a href="#">Account</a></li>
+                                <li><i class="fa fa-heart s_color" aria-hidden="true"></i><a href="#">Whishlist </a>
+                                </li>
+                                <li><i class="fa fa-truck s_color" aria-hidden="true"></i><a href="#">Whishlist</a></li>
+                            </ul>
+                            <div id="polyglotLanguageSwitcher">
+                                <form action="#">
+                                    <select id="polyglot-language-options">
+                                        <option id="en" value="en" selected>English</option>
+                                        <option id="fr" value="fr">French</option>
+                                        <option id="de" value="de">German</option>
+                                        <option id="it" value="it">Italian</option>
+                                        <option id="es" value="es">Spanish</option>
+                                    </select>
+                                </form>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div> <!-- End of .container -->
+            </div> <!-- End of .top_header -->
+
+            <div class="bottom_header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12 col-xs-12">
+                            <div class="search-box">
+                                <form action="#" class="clearfix">
+                                    <input type="text" placeholder="Search...">
+                                    <button><i class="fa fa-search"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-5 col-xs-6 logo-responsive">
+                            <div class="logo-area">
+                                <a href="index.php" class="pull-left logo"><img src="images/logo/logo.png"
+                                        alt="LOGO"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-7 col-xs-6 pdt-14">
+                            <div class="login_option float_left">
+                                <div class="login_form">
+                                    <div class="user">
+                                        <i class="icon-photo"></i>
+                                    </div>
+                                    <div class="login-info">
+                                        <div class="welcome">Welcome!</div>
+                                        <!-- select menu -->
+                                        <form action="#" class="select-form">
+                                            <div class="g-input f1 mb-30">
+                                                <!-- <select class="text-capitalize selectpicker" data-style="g-select" data-width="100%">
+							                                <option value="0" selected="">Sign In</option>
+															<option value="1">Sign In</option>
+							                                <option value="2">Register Here</option>
+							                            </select> -->
+                                                <a href="loginsignup.php">Sign In</a><br>
+                                                <a href="loginsignup.php">Register Here</a>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div> <!-- End of .cart_list -->
+                            </div>
+                            <div class="cart_option float_left">
+                                <button class="cart tran3s dropdown-toggle" id="cartDropdown"><i
+                                        class="fa icon-icon-32846" aria-hidden="true"></i><span
+                                        class="s_color_bg p_color">2</span></button>
+                                <div class="cart-info">
+                                    <div>My Cart</div>
+                                    <div class="doller">84.00$</div>
+                                </div>
+
+                                <div class="cart_list color2_bg" aria-labelledby="cartDropdown">
+                                    <ul>
+                                        <li>
+                                            <div class="cart_item_wrapper clear_fix">
+                                                <div class="img_holder float_left"><img src="images/shop/9.png"
+                                                        alt="Cart Image" class="img-responsive"></div>
+                                                <!-- End of .img_holder -->
+
+                                                <div class="item_deatils float_left">
+                                                    <h6>Turmeric Powde</h6>
+                                                    <ul>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                    </ul>
+                                                    <span class="font_fix">$ 34.99</span>
+                                                </div> <!-- End of .item_deatils -->
+                                            </div> <!-- End of .cart_item_wrapper -->
+                                        </li>
+
+                                        <li>
+                                            <div class="cart_item_wrapper clear_fix">
+                                                <div class="img_holder float_left"><img src="images/shop/10.png"
+                                                        alt="Cart Image" class="img-responsive"></div>
+                                                <!-- End of .img_holder -->
+
+                                                <div class="item_deatils float_left">
+                                                    <h6>Pure Jeans Coffee</h6>
+                                                    <ul>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                    </ul>
+                                                    <span class="font_fix">$ 26.99</span>
+                                                </div> <!-- End of .item_deatils -->
+                                            </div> <!-- End of .cart_item_wrapper -->
+                                        </li>
+
+                                        <li>
+                                            <div class="cart_item_wrapper clear_fix">
+                                                <div class="img_holder float_left"><img src="images/shop/11.png"
+                                                        alt="Cart Image" class="img-responsive"></div>
+                                                <!-- End of .img_holder -->
+
+                                                <div class="item_deatils float_left">
+                                                    <h6>Columbia Chocolate</h6>
+                                                    <ul>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></li>
+                                                    </ul>
+                                                    <span class="font_fix">$ 26.99</span>
+                                                </div> <!-- End of .item_deatils -->
+                                            </div> <!-- End of .cart_item_wrapper -->
+                                        </li>
+                                    </ul>
+
+                                    <div class="cart_total clear_fix">
+
+                                        <span class="total font_fix float_left">Total - 140$</span>
+                                        <a href="#" class="s_color_bg float_right tran3s">View Cart</a>
+                                    </div>
+                                </div> <!-- End of .cart_list -->
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div> <!-- End of .bottom_header -->
+        </header>
+
+
+
+
+
+        <!-- Menu ******************************* -->
+        <div class="theme_menu color1_bg">
+            <div class="container">
+                <nav class="menuzord pull-left" id="main_menu">
+                    <ul class="menuzord-menu">
+                        <li class="current_page"><a href="index.php">Home</a></li>
+                        <li><a href="about-us.php">About us</a></li>
+                        <!-- <li><a href="#">store</a>
+					      	<ul class="dropdown">
+					            <li><a href="shop.php">Shop With Sidebar</a></li>
+					            <li><a href="shop-single.php">Product Detail</a></li>
+					            <li><a href="shop-cart.php">Cart Page</a></li>
+					            <li><a href="checkout.php">Checkout Page</a></li>
+					            <li><a href="account.php">My Account</a></li>
+					         </ul> -->
+                        <!-- </li> -->
+                        <!-- <li><a href="#">News</a>
+					      	<ul class="dropdown">
+					            <li><a href="blog-Standard.php">News Standard</a></li>
+					            <li><a href="blog-list-sidebar.php">News With Sidebar</a></li>
+					            <li><a href="blog-grid.php">News Masonary</a></li>
+					            <li><a href="blog-details.php">News Single</a></li>
+					         </ul>
+					      </li> -->
+                        <!-- <li><a href="#">gallery</a>
+					      	<ul class="dropdown">
+					            <li><a href="gallery-grid.php">Gallery Grid</a></li>
+                                <li><a href="gallery-masonry.php">Gallery Masonary</a></li>
+                                <li><a href="gallery-single.php">Gallery Single</a></li>
+					         </ul>
+					      </li> -->
+
+                        <!-- <li><a href="#">Pages</a>
+					      	<ul class="dropdown">
+					            <li><a href="Testimonials.php">Testimonials</a></li>
+					            <li><a href="404.php">404v page</a></li>
+					            <li><a href="faq.php">FAQ</a></li>
+					         </ul>
+					      </li> -->
+
+                        <li><a href="contact.php">Contact us</a></li>
+                    </ul> <!-- End of .menuzord-menu -->
+                </nav> <!-- End of #main_menu -->
+
+
+                <!-- ******* Cart And Search Option ******** -->
+                <div class="nav_side_content pull-right">
+                    <ul class="icon_header">
+                        <li class="border_round tran3s"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li class="border_round tran3s"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li class="border_round tran3s"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li class="border_round tran3s"><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div> <!-- End of .nav_side_content -->
+
+            </div> <!-- End of .conatiner -->
+        </div> <!-- End of .theme_menu -->
